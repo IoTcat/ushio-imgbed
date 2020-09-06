4c25c9526783b4c2e3d6137f2e463a37854e3be4
iotcat <i@iotcat.me>
2020-09-06 18:46:20 +0800

    lang

--- js/main.js
+++ js/main.js
@@ -45,10 +45,10 @@ function select(){
 (function(){
     var options = {};
     $('.js-uploader__box').uploader({
-    	'selectButtonCopy': ((page.tran.getLang() == 'zh')?'请选择或拖拽文件':'Please select or drag and drop files'),
-    	'instructionsCopy': ((page.tran.getLang() == 'zh')?'你可以选择或拖拽多个文件':'You can select or drag multiple files'),
-    	'submitButtonCopy': ((page.tran.getLang() == 'zh')?'上传选择的文件':'Upload selected file'),
-    	'furtherInstructionsCopy': ((page.tran.getLang() == 'zh')?'你可以选择或拖拽更多的文件':'You can select or drag and drop more files'),
-    	'secondarySelectButtonCopy':((page.tran.getLang() == 'zh')?'选择更多的文件':'Select more files')
+    	'selectButtonCopy': '<span class=".zh">请选择或拖拽文件</span><span class=".en">Please select or drag and drop files</span>',
+    	'instructionsCopy': '<span class=".zh">你可以选择或拖拽多个文件</span><span class=".en">You can select or drag multiple files</span>',
+    	'submitButtonCopy': '<span class=".zh">上传选择的文件</span><span class=".en">Upload selected file</span>',
+    	'furtherInstructionsCopy': '<span class=".zh">你可以选择或拖拽更多的文件</span><span class=".en">You can select or drag and drop more files</span>',
+    	'secondarySelectButtonCopy':'<span class=".zh">选择更多的文件</span><span class=".en">Select more files</span>'
     });
 }());
